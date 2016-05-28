036da2b29fb44113b9f76c6cc28657962692522a
Alexandra Rodriguez <ale.tunn9095@gmail.com>
2016-05-27 23:23:50 -0400

    test project added

---
+++ Test/src/test/Person.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author PC_
+ */
+public class Person {
+    String name;
+
+    public Person(String personName) {
+        name = personName;
+    }
+
+    public String greet(String yourName) {
+        return String.format("Hi %s, my name is %s", name, yourName);
+    }
+}
